2d8f7bd8b8dc9c700a79a4a340c7e33641c54d8d
Sc-Cl <ScottyTCollins@Gmail.com>
2021-02-11 14:31:00 -0500

    Assignment two, actually pretty pleased with this one!

---
+++ bash/hostname-changer.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# This script is for the bash lab on variables, dynamic data, and user input
+# Download the script, do the tasks described in the comments
+# Test your script, run it on the production server, screenshot that
+# Send your script to your github repo, and submit the URL with screenshot on Blackboard
+# Get the current hostname using the hostname command and save it in a variable
+# Tell the user what the current hostname is in a human friendly way
+x=hostname
+$x
+# Tell the user what the current hostname is in a human friendly way
+echo "your current hostname is $HOSTNAME"
+# Ask for the user's student number using the read command
+echo -n "Enter your student number and press [ENTER]: ";
+read sn
+echo your Student number is ${sn}
+# Use that to save the desired hostname of pcNNNNNNNNNN in a variable, where NNNNNNNNN is the student number entered by the user
+# If that hostname is not already in the /etc/hosts file, change the old hostname in that file to the new name using sed or something similar and
+#     tell the user you did that
+#e.g. sed -i "s/$oldname/$newname/" /etc/hosts
+sudo sed -i "s/$HOSTNAME/$sn/" /etc/hosts
+hostnamectl set-hostname pc$sn
+# If that hostname is not the current hostname, change it using the hostnamectl command and
+#     tell the user you changed the current hostname and they should reboot to make sure the new name takes full effect
+#e.g. hostnamectl set-hostname $newname
+echo "hostname changed to pc$sn"
